200c0119b73033e7f42137016691f5a5929d264f
vishvaa6 <vishvaav006@gmail.com>
2024-07-07 17:38:11 +0530

    Delete question_1.py

--- question_1.py
+++
@@ -1,8 +0,0 @@
-numbers = [10, 501, 22, 37, 100, 999, 87, 351]
-
-even_numbers = [num for num in numbers if num % 2 == 0]
-odd_numbers = [num for num in numbers if num % 2 != 0]
-
-print("Even Numbers:", even_numbers)
-print("Odd Numbers:", odd_numbers)
-print(numbers)
